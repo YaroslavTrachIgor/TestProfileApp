242c76e8cd00818376aa13c08b7cac731333c5f8
Yaroslav Trach <zhbr282@gmail.com>
2024-02-06 23:03:25 -0500

    Initial Commit

---
+++ TestProfileApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestProfileApp
+//
+//  Created by User on 2024-02-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestProfileApp/TestProfileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProfileAppApp.swift
+//  TestProfileApp
+//
+//  Created by User on 2024-02-06.
+//
+
+import SwiftUI
+
+@main
+struct TestProfileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
